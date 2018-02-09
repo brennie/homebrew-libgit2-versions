adc45c61ac5eeb73377e0c16748cc9ddb56ecb3c
Barret Rennie <barret@brennie.ca>
2018-02-08 23:47:52 -0600

    libgit2@0.24: 0.24.6

---
+++ Formula/libgit2@0.24.rb
@@ -0,0 +1,58 @@
+class Libgit2AT024 < Formula
+  desc "C library of Git core methods that is re-entrant and linkable"
+  homepage "https://libgit2.github.com/"
+  url "https://github.com/libgit2/libgit2/archive/v0.24.6.tar.gz"
+  sha256 "af844f1708492fec7123cf1771275bc52e1b393e78f6f69689fbfd8cf297ac5b"
+  head "https://github.com/libgit2/libgit2.git"
+
+  option :universal
+
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "libssh2" => :recommended
+  depends_on "openssl" if MacOS.version <= :lion # Uses SecureTransport on >10.7
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_EXAMPLES=YES"
+    args << "-DBUILD_CLAR=NO" # Don't build tests.
+    args << "-DUSE_SSH=NO" if build.without? "libssh2"
+
+    if build.universal?
+      ENV.universal_binary
+      args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
+    end
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      cd "examples" do
+        (pkgshare/"examples").install "add", "blame", "cat-file", "cgit2",
+                                      "describe", "diff", "for-each-ref",
+                                      "general", "init", "log", "remote",
+                                      "rev-list", "rev-parse", "showindex",
+                                      "status", "tag"
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <git2.h>
+
+      int main(int argc, char *argv[]) {
+        int options = git_libgit2_features();
+        return 0;
+      }
+    EOS
+    libssh2 = Formula["libssh2"]
+    flags = %W[
+      -I#{include}
+      -I#{libssh2.opt_include}
+      -L#{lib}
+      -lgit2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
